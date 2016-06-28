8a70e86fc35e00127b75b777b78f245886306a3a
FIXDISC <acavieres@hotmail.com>
2016-06-28 19:54:37 -0400

    Testing

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,34 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/phonegap-plugin-barcodescanner/www/barcodescanner.js",
+        "id": "phonegap-plugin-barcodescanner.BarcodeScanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/android/notification.js",
+        "id": "org.apache.cordova.dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.2",
+    "phonegap-plugin-barcodescanner": "5.0.0",
+    "org.apache.cordova.dialogs": "0.3.0",
+    "cordova-plugin-compat": "1.0.0"
+}
+// BOTTOM OF METADATA
+});
